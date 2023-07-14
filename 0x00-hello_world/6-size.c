2fe28726abc55bafa011c4902d23e5164f25bc7c
SEUNAYO040 <ayomiposiaderonke@gmail.com>
2023-07-14 01:24:06 -0700

    huge

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,14 @@
 #include < stdio.h >
-
+/**
+ * main - This program prints the size of various computer types
+ * Return: 0 (SUCCESS)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+printf(
 
