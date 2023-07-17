5aa03223f4770b0eb3c7705b315dbf789e6065b3
SEUNAYO040 <ayomiposiaderonke@gmail.com>
2023-07-17 04:44:02 -0700

    joy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include < stdio.h >
+#include <stdio.h>
 /**
  * main - This program prints a line with the printf function
  * Return: 0 (SUCCESS)
@@ -8,4 +8,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
-
