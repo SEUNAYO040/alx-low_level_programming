bf779a4e46147ec212f0fd1730ba06fefdc9bf27
SEUNAYO040 <ayomiposiaderonke@gmail.com>
2023-07-17 04:38:02 -0700

    many

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 /**
  * main - This program prints line with the puts function
  * Return: 0 (SUCCCESS)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
