505f0d9e2dc4d26bbb00eb8c3936b4b9633cf190
SEUNAYO040 <ayomiposiaderonke@gmail.com>
2023-07-18 22:09:01 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *print_alphabet - Print lowercase alphabet
+  *Return: 0
+  */
+void print_alphabet(void)
+{
+	int i:
+		for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+	return (0);
+}
